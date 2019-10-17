b91d3cf1c9dcb93cf04b46a40efde3aad71d2fac
Florian Friedl <20150066@htblaperg.onmicrosoft.com>
2019-10-17 07:46:00 +0200

    03_Friedl

---
+++ HUE02_Friedl_trippin/HUE02_Friedl_trippin/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace HUE02_Friedl_trippin
+{
+    public class People
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string CityName { get; set }
+        public string Country { get; set; }
+    }
+}
---
+++ HUE02_Friedl_trippin/HUE02_Friedl_trippin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HUE02_Friedl_trippin
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await Get();
+        }
+
+        List<People> allUsers;
+        private static HttpClient HttpClient
+            = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/") };
+
+        public static async Task Get()
+        {
+            var userResponse = await HttpClient.GetAsync("People");
+            userResponse.EnsureSuccessStatusCode();
+            var responseBody = await userResponse.Content.ReadAsStringAsync();
+            var users = JsonSerializer.Deserialize<Peoples>(responseBody);
+            users.Results.ForEach(u => Console.WriteLine(u.UserName));
+        }
+    }
+}
